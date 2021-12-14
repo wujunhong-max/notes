991ebb016d51dc50e78d2ee0f530be847bbe2471
wujunhong-max <209020568@qq.com>
2021-12-14 23:37:07 +0800

    update

--- 9. leetcode/Demo/test.cpp
+++ 9. leetcode/Demo/test.cpp
@@ -5,68 +5,44 @@
 #include<vector>
 using namespace std;
 
+class CustomStack{
+public:
+    vector<int> stk;
+    int top;
 
-int main()
-{
-    string  s = "loveleetcode";
-    char c = 'e';
-
-    vector<int> answer;
-    vector<int> a;
-    // 将相同字符的位置放进数组a中
-    for(int i=0; i<s.length(); i++)
+    CustomStack(int maxSize)
     {
-        if(s[i] == c)
-        {
-            a.push_back(i);
-        }
-    } 
-    int k = a.size();
-
-    for(vector<int>::iterator it = a.begin(); it != a.end();it++)
-    {
-        cout << *it << " ";  // 3 5 6 11
+        stk.resize(maxSize);
+        top = -1;
     }
-    cout << endl;
-    cout << k << endl;
-    int x=0, y=0;
-    // 遍历数组answer
-    for(int i=0; i<s.size(); i++)
+    void push(int x)
     {
-        if(i+1>a[y+1])
-            y++;
-        if(k <= 0)  break;
-        if(k>0 && i == a[x])
-        {
-            answer.push_back(0);
-            if(x<k-1)
-                x++;
-        }
-        else if(k>0 && i<a[0])
-        {
-            answer.push_back(abs(i-a[0]));
-        }
-        else if(k>1 && a[0] < i < a[k-1])
-        {
-            int num = min(abs(i-a[y]), abs(a[y+1]-i));
-            answer.push_back(num);
-            if(i==7) 
-            {
-                cout << y<<  " " <<abs(i-a[y]) << " " << abs(a[y+1]-i) << " " << num <<endl;
-            }
-            
-            num = 0;
-        }
-        else if(k>0 && i>a[k-1])
-        {
-            answer.push_back(abs(i-a[k-1]));
+        if(top != stk.size()-1)
+        {   
+            top++;
+            stk[top] = x;
         }
     }
-    cout << endl;
-    for(vector<int>::iterator it = answer.begin(); it != answer.end();it++)
+    int pop()
     {
-        cout << *it << " ";  // 3 5 6 11
+        if(top == -1)
+        {
+            return -1;
+        }
+        top--;
+        return stk[top + 1];
     }
-    cout << endl;
-    return 0;
-}
+    void increment(int k, int val)
+    {
+        int lim = min(k, top+1);
+        for(int i=0; i<lim; i++)
+        {
+            stk[i] += val;
+        }
+    }
+    
+public:
+    vector<int> stk;
+    int top;    
+};
+ 
